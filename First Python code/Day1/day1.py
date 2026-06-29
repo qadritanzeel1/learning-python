fdafc430bb5aef3f22f07dc44077f2ccef8c673c
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-06-29 11:31:37 +0530

    Added Day 1, Day 2 and Day 3 Python practice

---
+++ First Python code/Day1/day1.py
@@ -0,0 +1,7 @@
+#sum of two numbers
+
+a = 10 
+b = 20
+c= a+b
+
+print(c)
---
+++ First Python code/Day3/main.py
@@ -0,0 +1,37 @@
+#WAP to input user's first name and print its length
+
+# name = input("Please enter a name");
+
+# print(len(name));
+# print(name.count("$"))
+
+# WAP to check if a number entered by the user is even or odd
+
+# num = int(input("Please enter a number"));
+
+# if(num%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+
+# WAP to find thegreatest of three numbers entered by the user
+
+# num1 = int(input("Please enter 1st number"))
+# num2 = int(input("Please enter 2nd number"))
+# num3 = int(input("Please enter 3rd number"))
+
+# if(num1>num2 and num1>num3) :
+#     print("1st number is greater")
+# elif(num2>num1 and num2>num3):
+#     print("2nd is greater")
+# else:
+#     print("3rd is greater")
+
+
+number = int(input("Please enter a number"))
+
+if(number%7==0) :
+    print("Number is multiple of 7")
+else:
+    print("Number is not multiple of 7")
