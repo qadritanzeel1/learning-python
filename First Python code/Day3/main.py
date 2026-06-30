9f45104c2b2dee630d0137e98d41bf7c26683063
qadritanzeel1 <tanzeelqadri042@gmail.com>
2026-06-30 11:11:39 +0530

    dictionaries and sets in python

--- First Python code/Day3/main.py
+++ First Python code/Day3/main.py
@@ -29,9 +29,50 @@
 #     print("3rd is greater")
 
 
-number = int(input("Please enter a number"))
+# number = int(input("Please enter a number"))
 
-if(number%7==0) :
-    print("Number is multiple of 7")
-else:
-    print("Number is not multiple of 7")
+# if(number%7==0) :
+#     print("Number is multiple of 7")
+# else:
+#     print("Number is not multiple of 7")
+
+
+#WAP to ask the user to enter names of their 3 favourite movies and store them in a list .
+
+# movies = []
+
+# movies.append(input("Enter first fruit : "))
+# movies.append(input("Enter second fruit : "))
+# movies.append(input("Enter third fruit : "))
+
+# print(movies)
+
+
+
+#WAP to check if a list contains a palindrome of elements 
+
+# list = ["m","a","a","m"]  #palindrome
+#list = ["m","a","a","n"]  #not palindrome
+
+# copy_list = list.copy()
+# copy_list.reverse()
+
+# if(list==copy_list):
+#     print("Palindrome")
+# else:
+#     print("Not palindrome")
+
+
+# WAP to count the number of students with the A grade in the following tuple
+
+# grade = ("C", "B", "A", "A", "D", "A");
+
+
+# print(grade.count("A"))
+
+#store the above values in a list and sort them from A to D
+grade = ["C", "B", "A", "A", "D", "A"]
+
+grade.sort()
+
+print(grade)
